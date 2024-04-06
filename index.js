2d75405559828330dfbe9639b200f08c2f665403
Mayc0n18 <150074417+Mayc0n18@users.noreply.github.com>
2024-04-06 18:54:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-function mostrarTexto() {
-  var overflow = document.getElementById('overflow');
-  overflow.style.display = 'block';
-}
+$(document).ready(function() {
+  $(".textBox").click(function() {
+    $("#overflow").toggle();
+    if ($("#overflow").is(":visible")) {
+      $("#overflow").css({
+        'top': '150px',
+      'left': '10px'
+    
+      });
+      $('.textBox').css('height', '780px');
+    } else {
+      $("#overflow").css('display', 'none');
+      $('.textBox').css('height', ''); 
+    }
+  });
+});
+$(document).ready(function(){
+  $('.topic').click(function(){
+    $('.subDisc').hide();
+    $('.pass').show();
+  })
+})
+$(document).ready(function(){
+  $('.send').click(function(){
+    $('.pass').hide();
+    $('.last').show();
+    $('.discBar').css({
+      'top':'-35px'
+    });
+  })
+})

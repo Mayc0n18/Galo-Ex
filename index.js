a1bc134428176239dc1f4c8142e5612ec2eb59c8
Mayc0n18 <150074417+Mayc0n18@users.noreply.github.com>
2024-04-06 21:15:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".textBox").click(function() {
     $("#overflow").toggle();
+    $('.toggle').toggle();
     if ($("#overflow").is(":visible")) {
       $("#overflow").css({
         'top': '150px',
@@ -31,12 +32,41 @@ $(document).ready(function(){
       'top':'55px',
       'left': '50px'
     });
+    $('.comentI').addClass('blur');
+
   })
 })
 $(document).ready(function(){
   $('.create').click(function(){
+    $('.comentI').hide();
     $('.last').hide();
     $('.pass').show();
   })
  
 })
+$(document).ready(function(){
+  $('.coment2').click(function(){
+    $('.ans').toggle();
+  })
+  $('.ans').click(function(){
+    $('.ans').toggle();
+  })
+})
+
+$(document).ready(function() {
+  // Variável para armazenar o texto original
+  var originalText = $('.coment2 ul li p').text();
+
+  // Adiciona um evento de clique ao elemento com a classe .dttt
+  $('.coment2').click(function() {
+      // Verifica se o texto atual é o original
+      if ($('.coment2 ul li p').text() === originalText) {
+          // Se sim, muda para o novo texto
+          $('.coment2 ul li p').text("5 like 5 resposta");
+      } else {
+          // Se não, volta para o texto original
+          $('.coment2 ul li p').text(originalText);
+      }
+  });
+  
+});
